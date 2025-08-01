634e99e2dd73f1281f15ceb2572224b41c1566b9
Akash Biswas <akash41bt@gmail.com>
2025-08-02 00:36:43 +0600

    solve LeetCode 70: using bottom-up dp

--- 70-climbing-stairs.cpp
+++ 70-climbing-stairs.cpp
@@ -1,17 +1,18 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> dp;
-
-    Solution(){
-        dp.resize(46, -1);
-    }
-
-    int climbStairs(int n) {
-        if(n == 1 || n == 2)
+    int climbStairs(int n)
+    {
+        if (n == 1 || n == 2)
             return n;
-        if(dp[n] != -1)
-            return dp[n];
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        dp[2] = 2;
 
-        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
