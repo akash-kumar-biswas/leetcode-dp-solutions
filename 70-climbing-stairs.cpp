0d5204a395107652c7ad7d7dbb78624e73bdd952
Akash Biswas <akash41bt@gmail.com>
2025-08-02 00:29:20 +0600

    solve LeetCode 70: recursion + memoization dp

---
+++ 70-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    Solution(){
+        dp.resize(46, -1);
+    }
+
+    int climbStairs(int n) {
+        if(n == 1 || n == 2)
+            return n;
+        if(dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
