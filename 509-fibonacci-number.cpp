1ea14f2faee2652f5c8582f5e30fda893aff2df0
Akash Biswas <akash41bt@gmail.com>
2025-08-01 12:07:04 +0600

    solve LeetCode 509: using bottom-up dp

--- 509-fibonacci-number.cpp
+++ 509-fibonacci-number.cpp
@@ -1,17 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> dp;
-
-    Solution(){
-        dp.resize(31, -1);
-    }
-
-    int fib(int n) {
-        if(n == 0 || n == 1)
+    int fib(int n)
+    {
+        if (n == 0 || n == 1)
             return n;
-        if(dp[n] != -1)
-            return dp[n];
 
-        return dp[n] = fib(n - 1) + fib(n - 2);
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
