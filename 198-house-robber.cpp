462826053dd62534919d18e344e655ffd8754b81
Akash Biswas <akash41bt@gmail.com>
2025-08-02 18:43:13 +0600

    solve LeetCode 198: using bottom-up dp

--- 198-house-robber.cpp
+++ 198-house-robber.cpp
@@ -1,22 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> dp;
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
 
-    int solve(vector<int>& nums, int idx, int n){
-        if(idx >= n)
-            return 0;
+        vector<int> dp(n + 1);
 
-        if(dp[idx] != -1)
-            return dp[idx];
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        int rob = nums[idx] + solve(nums, idx + 2, n);
-        int do_not_rob = solve(nums, idx + 1, n);
-        
-        return dp[idx] = max(rob, do_not_rob);
-    }
-
-    int rob(vector<int>& nums) {
-        dp.resize(101, -1);
-        return solve(nums, 0, nums.size());
+        for (int i = 2; i <= n; i++)
+        {
+            dp[i] = max(nums[i - 1] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[n];
     }
 };
