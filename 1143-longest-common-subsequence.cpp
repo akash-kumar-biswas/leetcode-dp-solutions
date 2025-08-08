03533b0c93aee681001fa15b1dabb042fa8abe9f
Akash Biswas <akash41bt@gmail.com>
2025-08-08 15:08:14 +0600

    solve LCS problem using top-down dp

--- 1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence.cpp
@@ -1,22 +1,33 @@
-class Solution {
+class Solution
+{
 public:
+    vector<vector<int>> dp;
 
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size();
-        int n = text2.size();
+    int solve(string text1, string text2, int i, int j)
+    {
+        if (i >= text1.size() || j >= text2.size())
+            return 0;
 
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(text1[i-1] == text2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
-                }
-            }
+        if (text1[i] == text2[j])
+        {
+            return dp[i][j] = 1 + solve(text1, text2, i + 1, j + 1);
         }
-        return dp[m][n];
+        else
+        {
+            return dp[i][j] = max(solve(text1, text2, i, j + 1),
+                                  solve(text1, text2, i + 1, j));
+        }
+    }
+
+    int longestCommonSubsequence(string text1, string text2)
+    {
+
+        int n1 = text1.size(), n2 = text2.size();
+        dp.resize(n1 + 1, vector<int>(n2 + 1, -1));
+
+        return solve(text1, text2, 0, 0);
     }
 };
